a15e6dde63c0e978bbf482ad0bbe80a3800688b8
npadmanabhan05 <nitya.d.padmanabhan@gmail.com>
2025-07-25 11:35:11 -0400

    claim extraction functionality

---
+++ claim_extraction/extracting_claims.py
@@ -0,0 +1,51 @@
+import os
+from db.db_utils import fetch_no_claim_articles, set_claims, fetch_claims
+from dotenv import load_dotenv
+from openai import OpenAI
+load_dotenv()
+
+API_KEY = os.getenv("OPEN_AI_KEY")
+client = OpenAI(api_key = API_KEY)
+# articles here is a list of various tuples that represent the individual articles
+articles = fetch_no_claim_articles()
+print(len(articles))
+print ("\n")
+for article in articles: 
+    id = article[0]
+    text = article[1]
+    response = client.chat.completions.create(
+        model = "gpt-4.1",
+        messages = [
+            # {
+            #     "role": "system", 
+            #     "content": "You are an economic-claim extractor. Please only extract economic claims made by the author of the article. Avoid refrencing quotes from the president or other individuals"
+            # },
+            {
+                "role": "user", 
+                "content": f"""From the following article, can you extract all of the claims that the author or the news outlet itself makes about tariffs or the economy. These claims should cover areas such as the current state of the economy, the effects of tariffs, etc. Please focus on the claims made by the author or news outlet and avoid claims made about what trump or other officials are saying. Please provide me a list of claims (as a list of strings). This is how the output should look: 
+
+                            [
+                                claim 1,
+                                claim 2,
+                                etc.
+                            ]
+
+                            Here is the article: 
+
+                                {text}
+                            """
+            }
+        ]
+    )
+    claims = response.choices[0].message.content
+    print(f"response for article {id}: ")
+    print(claims)
+    print("\n\n\n")
+    set_claims(id, claims)
+
+print("claims for article 1: ")
+print(fetch_claims(1))
+print("\n\n\n")
+print("claims for article 2: ")
+print(fetch_claims(2))
+
--- db/db_utils.py
+++ db/db_utils.py
@@ -30,13 +30,15 @@ def create_table():
     connection.close()
 
 def insert_articles(data, political_leaning):
-    # retreives an list of dictionaries representing the various articles returned by the GNews API
+    # retreives an list of dictionaries representing the various articles returned by the News API
     articles = data.get("articles", [])
+    # checks if any articles were returned
     if not articles: 
         print("no articles found")
         return
     connection = getConnection()
     cursor = connection.cursor()
+    # attemps to insert each article into the database
     for article in articles: 
         try:
             cursor.execute('''
@@ -79,3 +81,48 @@ def fetch_all_articles():
         for row in rows:
             writer.writerow(row)
     print(f"Exported {len(rows)} articles to exported_articles.csv")
+    connection.commit()
+    connection.close()
+
+def fetch_no_claim_articles():
+    connection = getConnection()
+    cursor = connection.cursor()
+    cursor.execute("SELECT article_id, content from articles WHERE claims IS NULL")
+    unchecked_articles = cursor.fetchall()
+    if not unchecked_articles: 
+        print("None")
+    connection.commit()
+    connection.close()
+    return unchecked_articles
+    
+
+def add_content(id, content):
+    connection = getConnection()
+    cursor = connection.cursor()
+    cursor.execute('''
+       UPDATE articles
+       SET content = ?
+       WHERE article_id = ?
+    ''', (content, id))
+    connection.commit()
+    connection.close()
+
+def set_claims(id, claims):
+    connection = getConnection()
+    cursor = connection.cursor()
+    cursor.execute('''
+        UPDATE articles
+        SET claims = ?
+        WHERE article_id = ?
+    ''', (claims, id))
+    connection.commit()
+    connection.close()
+
+def fetch_claims(id):
+    connection = getConnection()
+    cursor = connection.cursor()
+    cursor.execute('''
+        SELECT claims FROM articles WHERE article_id = ?
+    ''', (id,))
+    claims = cursor.fetchone()
+    return claims
